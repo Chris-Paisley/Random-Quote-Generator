4b9e88aa613cf53bcd8ce6a9109707ab276caf68
Chris Paisley <lkenn1990@hotmail.com>
2018-10-02 22:15:06 -0700

    Added some quotes to my quoet array

--- js/script.js
+++ js/script.js
@@ -1,15 +1,61 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
-let quotes = [];
+
+let quotes = [
+  {
+    quote: “You don’t love because:
+      you love despite; not for the virtues, but despite the faults.”,
+    source: "William Faulkner",
+    citation: "Mississippi",
+    year: 1954,
+    tags: ["love", "essay"]
+  },
+  {
+    quote:  “Love is the voice under all silences, the hope which has no opposite
+      in fear; the strength so strong mere force is feebleness:
+      the truth more first than sun, more last than star.”,
+    source:  "e.e. cummings",
+    citation: "being to timelessness as it’s to time",
+    year: 1958,
+    tags: ["love", "poem"]
+  },
+  {
+    quote: "I have not failed. I have just found 10,000 things that do not work.",
+    source: "Thomas Edison"
+    tags: "inspirational"
+  },
+  {
+    quote:
+    source:
+    citation:
+    year:
+  },
+  {
+    quote:
+    source:
+    citation:
+    year:
+  },
+  {
+    quote:
+    source:
+    citation:
+    year:
+  },
+];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
+function getRandomQuote (){
 
+}
 
 // Create the printQuote funtion and name it printQuote
+function printQuote (){
 
+}
 
 
 // This event listener will respond to "Show another quote" button clicks
