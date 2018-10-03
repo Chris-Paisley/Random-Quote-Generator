d43e0ff1fad340893d18478aa89435ff542d673d
Chris Paisley <lkenn1990@hotmail.com>
2018-10-03 00:32:48 -0700

    added printQuote function, its not working

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@ let quotes = [
     source: "William Faulkner",
     citation: "Mississippi",
     year: 1954,
-    tags: ["love", "essay"]
+    tags: "love"
   },
   {
     quote:  "Love is the voice under all silences, the hope which has no opposite in fear; the strength so strong mere force is feebleness the truth more first than sun, more last than star.",
     source:  "e.e. cummings",
     citation: "being to timelessness as it’s to time",
     year: 1958,
-    tags: ["love", "poem"]
+    tags: "love",
   },
   {
     quote: "I have not failed. I have just found 10,000 things that do not work.",
@@ -37,19 +37,42 @@ let quotes = [
     source: "William Shakespeare",
     citation: "Romeo & Juliet – Act 1, Scene 1",
     year: 1597,
-    tags: ["love", "play"]
+    tags: "love"
   },
 ];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
-function getRandomQuote (){
+//Gets random quote from quotes array
+function getRandomQuote (array){
+  let numb = Math.floor(Math.random() * array.length);
+  return array[numb];
 
 }
 
 // Create the printQuote funtion and name it printQuote
+
+//prints a random quote to the page
 function printQuote (){
+  let randNumb = getRandomQuote(array);
+  let quotePrinted = '';
+
+  quotePrinted += '<p class="quote">' + quotes[randNumb].name + '</p>';
+  quotePrinted += '<p class="source">' + quotes[randNumb].source;
+
+  //only including citation, year and tags if they exist otherwise it ends with they name and source
+  if (quotes[randNumb].citation){
+    quotePrinted += '<span class="citation">' + quotes[randNumb].citation + '</span>';
+  }else if (quotes[randNumb].year){
+    quotePrinted += '<span class="year">' + quotes[randNumb].year + '</span>';
+  } else if (quotes[randNumb].tags) {
+    quotePrinted += '<span class="tags">' + quotes[randNumb].tags + '</span>';
+  } else {
+    quotePrinted += '</p>'
+  }
+
+  document.getElementById('quote-box').innerHTML = stringOfQuoteProperties;
 
 }
 
