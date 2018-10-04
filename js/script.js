ffa799a6103806e496ebe8de33cad0f7492897a6
Chris Paisley <lkenn1990@hotmail.com>
2018-10-03 18:27:16 -0700

    got it to work, added comments, it now displays tags, year and citation

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ let quotes = [
     quote: "You don’t love because: you love despite; not for the virtues, but despite the faults.",
     source: "William Faulkner",
     citation: "Mississippi",
-    year: 1954,
+    year: "1954",
     tags: "love"
   },
   {
-    quote:  "Love is the voice under all silences, the hope which has no opposite in fear; the strength so strong mere force is feebleness the truth more first than sun, more last than star.",
-    source:  "e.e. cummings",
-    citation: "being to timelessness as it’s to time",
-    year: 1958,
+    quote:  "A loving heart is the truest wisdom.",
+    source:  "Charles Dickens",
+    citation: "David Copperfield",
+    year: "1849",
     tags: "love",
   },
   {
@@ -36,7 +36,7 @@ let quotes = [
     quote: "Love is a smoke and is made with the fume of sighs.",
     source: "William Shakespeare",
     citation: "Romeo & Juliet – Act 1, Scene 1",
-    year: 1597,
+    year: "1597",
     tags: "love"
   },
 ];
@@ -48,39 +48,42 @@ let quotes = [
 function getRandomQuote (array){
   let numb = quotes[Math.floor(Math.random() * quotes.length)];
   return numb;
-
 }
-
-//testing to see if this works
-console.log(getRandomQuote());
-// Create the printQuote funtion and name it printQuote
 
 //prints a random quote to the page
 function printQuote (){
   let randNumb = getRandomQuote();
+  //Empty string to build the new string of quote, source, year and citation
   let quotePrinted = '';
 
+  //Adding the random quote and its source to the quotePrinted string
   quotePrinted += '<p class="quote">' + randNumb.quote + '</p>';
   quotePrinted += '<p class="source">' + randNumb.source;
 
-  //only including citation, year and tags if they exist otherwise it ends with they name and source
-  if (randNumb.citation){
-    quotePrinted += '<span class="citation">' + randNumb.citation + '</span>';
-  }else if (randNumb.year){
-    quotePrinted += '<span class="year">' + randNumb.year + '</span>';
-  } else if (randNumb.tags) {
-    quotePrinted += '<span class="tags">' + randNumb.tags + '</span>';
+  //only including citation, year and tags if they exist otherwise it ends with the quote name and source
+  if (randNumb.hasOwnProperty('citation')){
+    quotePrinted += '<span class="citation"> ' + randNumb.citation + '</span>';
+    if (randNumb.hasOwnProperty('year')){
+      quotePrinted += '<span class="year"> ' + randNumb.year + '</span>';
+    }
+    if (randNumb.hasOwnProperty('tags')) {
+      quotePrinted += '<p class="tags"> #' + randNumb.tags + '</p>';
+    }
+  }else if (randNumb.hasOwnProperty('tags')) {
+
+    quotePrinted += '<p class="tags"> #' + randNumb.tags + '</p>';
   } else {
     quotePrinted += '</p>'
+    return quotePrinted;
   }
+
   document.getElementById('quote-box').innerHTML = quotePrinted;
-
-
-
-}
+} //End of printQuote function
 
 //print quote to the page
 printQuote();
+
+
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
